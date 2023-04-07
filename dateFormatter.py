900271d190f1ea67f9eda5a4f3f483abf871ebe6
sandunrmst <sandunalugc@gmail.com>
2023-04-07 13:47:06 +0530

    add all

---
+++ dateFormatter.py
@@ -0,0 +1,26 @@
+
+def dateFormatter(date):
+
+    year = ""
+    month = ""
+    day = ""
+    split_list=[]
+    new_Date = ""
+
+    tempHolder = date.split("/")
+    
+    year = tempHolder[2]
+    month = tempHolder[1]
+    day = tempHolder[0]
+
+    split_list.append(year)
+    split_list.append(month)
+    split_list.append(day)
+    
+    new_Date = "/".join(split_list)
+
+    return "Befor -> "+ date + " After -> " + new_Date
+
+print(dateFormatter("12/05/2023"))
+
+        
