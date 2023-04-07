c46bed0caac93e320f52b69337063ae1320ce9d0
sandunrmst <sandunalugc@gmail.com>
2023-04-07 15:04:44 +0530

    Date formater advanced

--- formatterAdvanced.py
+++ formatterAdvanced.py
@@ -1,4 +1,4 @@
-import string
+import string #easy way for handle strings 
 
 lower = string.ascii_lowercase #simply you can try this for validate the date if user enter the letters with date 
 upper = string.ascii_uppercase #simply you can try this for validate the date if user enter the letters with date 
@@ -57,9 +57,6 @@ def dateFormatter(date):
     else:
         print("please Enter valid date like in example e.g -> 08/04/2022")
         
-
-            
-
 user_input = str(input("Enter your date formate: e.g-> 12/05/2023 : "))
  
 print(dateFormatter(user_input))
